28e73cac1992fae5c490f098e2cf7a42327c4e57
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-10-03 08:33:38 +0100

    fix:allowed host

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,11 +11,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = config(
-    "DJANGO_ALLOWED_HOSTS",
-    default="greencart-api-qs0a.onrender.com,www.yourdomain.com",
-    cast=lambda v: [host.strip() for host in v.split(",")]
-)
+ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", cast=list)
 
 # DATABASES
 # ------------------------------------------------------------------------------
