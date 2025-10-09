f46e09e570d8359af66c022c6de02eb448a940d1
loic-houchi <houchiloictchoula@gmail.com>
2025-10-09 10:22:33 +0100

    application order finished order and cart

--- green_cart_api/cart/urls.py
+++ green_cart_api/cart/urls.py
@@ -9,10 +9,11 @@ app_name = 'carts'
 
 urlpatterns = [
     path('my-cart/', MyCartView.as_view(), name='my-cart'),
-    path('<int:pk>/add-item/', AddItemView.as_view(), name='add-item'),
-    path('<int:pk>/remove-item/', RemoveItemView.as_view(), name='remove-item'),
-    path('<int:pk>/update-quantity/', UpdateQuantityView.as_view(), name='update-quantity'),
-    path('<int:pk>/clear/', ClearCartView.as_view(), name='clear-cart'),
+    path('<uuid:pk>/add-item/', AddItemView.as_view(), name='add-item'),
+    path('<uuid:pk>/remove-item/', RemoveItemView.as_view(), name='remove-item'),
+    path('<uuid:pk>/update-quantity/', UpdateQuantityView.as_view(), name='update-quantity'),
+    path('<uuid:pk>/clear/', ClearCartView.as_view(), name='clear-cart'),
     # Si vous voulez un endpoint pour récupérer les détails du panier (GET /<pk>/), ajoutez :
     # path('<int:pk>/', CartDetailView.as_view({'get': 'get'}), name='cart-detail'),
+    
 ]
--- green_cart_api/order/apps.py
+++ green_cart_api/order/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class OrderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'order'
+    name = 'green_cart_api.order'
